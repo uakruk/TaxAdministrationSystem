3e92f002b42c2e3836c204eb6534e1a9b607dd21
Alex Dobr <dobrunya777@gmail.com>
2015-04-13 18:12:42 +0000

    AnotherWay auth. We will check both of them

---
+++ app/scripts/controllers/signin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('TaxPayerApp')
+  .controller('SigninCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
