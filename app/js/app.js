c8c265af37a5d09444efdf586b1350a4715c13da
Alex Dobr <dobrunya777@gmail.com>
2015-04-28 20:19:49 +0000

    Piece of shit

--- app/js/app.js
+++ app/js/app.js
@@ -66,6 +66,85 @@ KursachApp.config(['$routeProvider',
                     templateUrl: 'partials/auth.html',
                     controller: 'UserListCtrl'
                   }).
+      when('/unit', {
+        templateUrl: 'partials/unit.html',
+        controller: 'UnitCtrl'
+      }).
+      when('/unit/:unit_id', {
+        templateUrl: 'partials/unit_id.html',
+        controller: 'UnitIdCtrl'
+      }).
+      when('/taxpayer/:id/tax', {
+        templateUrl: 'partials/tax.html',
+        controller: 'TaxpayerTaxCtrl'
+      }).
+        when('/taxpayer/:id/tax/:tax_id', {
+            templateUrl: 'partials/tax_id.html',
+            controller: 'TaxpayerTaxIdCtrl'
+        }).
+        when('/taxpayer/:id', {
+            templateUrl: 'partials/taxpayer.html',
+            controller: 'TaxpayerCtrl'
+        }).
+        when('/taxpayer/:id/taxitionobject', {
+            templateUrl: 'partials/taxpayer_objects.html',
+            controller: 'TaxpayerObjectsCtrl'
+        }).
+        when('/taxpayer/:id/taxitionobject/:taxition_id', {
+            templateUrl: 'partials/taxpayer_object.html',
+            controller: 'TaxpayerObjectCtrl'
+        }).
+        when('/taxpayer/:id/property', {
+            templateUrl: 'partials/taxpayer_properties.html',
+            controller: 'TaxpayerPropertiesCtrl'
+        }).
+        when('/taxpayer/:id/property/:property_id', {
+            templateUrl: 'partials/taxpayer_property.html',
+            controller: 'TaxpayerPropertyCtrl'
+        }).
+        when('/taxpayer/:id/tax/:tax_id/payment', {
+            templateUrl: 'partials/taxpayer_payments.html',
+            controller: 'TaxpayerPaymentsCtrl'
+        }).
+        when('/taxpayer/:id/tax/:tax_id/payment/:payment_id', {
+            templateUrl: 'partials/taxpayer_payment.html',
+            controller: 'TaxpayerPaymentCtrl'
+        }).
+        when('/taxpayer/:id/licence', {
+            templateUrl: 'partials/taxpayer_licences.html',
+            controller: 'TaxpayerLicencesCtrl'
+        }).
+        when('/taxpayer/:id/licence/:licence_id', {
+            templateUrl: 'partials/taxpayer_licence.html',
+            controller: 'TaxpayerLicenceCtrl'
+        }).
+        when('/taxpayer/:id/taxitionobject/:taxition_id/cacheregister', {
+            templateUrl: 'partials/taxpayer_cash.html',
+            controller: 'TaxpayerCashCtrl'
+        }).
+        when('/taxpayer/:id/tax/:tax_id/charge', {
+            templateUrl: 'partials/taxpayer_charges.html',
+            controller: 'TaxpayerChargesCtrl'
+        }).
+        when('/taxpayer/:id/tax/:tax_id/charge/:charge_id', {
+            templateUrl: 'partials/taxpayer_charge.html',
+            controller: 'TaxpayerChargeCtrl'
+        }).
+        when('/taxpayer/:id/audit', {
+            templateUrl: 'partials/taxpayer_audits.html',
+            controller: 'TaxpayerAuditsCtrl'
+        }).
+        when('/taxpayer/:id/audit/:audit_id', {
+            templateUrl: 'partials/taxpayer_audit.html',
+            controller: 'TaxpayerAuditCtrl'
+        }).
+        when('/taxpayer/:id/audit/:audit_id', {
+            templateUrl: 'partials/taxpayer_audit.html',
+            controller: 'TaxpayerAuditCtrl'
+        }).when('/taxpayer/:id/audit/:audit_id/decision', {
+            templateUrl: 'partials/taxpayer_decision.html',
+            controller: 'TaxpayerDecisionCtrl'
+        }).
       when('/login', {
                           templateUrl: 'partials/login.html',
                           controller: 'LoginCtrl'
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -23,13 +23,157 @@
 
 var KursachControllers = angular.module('KursachControllers',[]);
 
-KursachControllers.controller('UserProfileCtrl',['$scope','$http',
-  function ($scope, $http) {
-      $http.get('users/users2.json').success(function(data) {
-        $scope.users = data;
-      });
-      $scope.orderProp = 'age';
-  }]);
+KursachControllers.controller('UnitCtrl',['$scope','$http',
+    function ($scope, $http) {
+        $http.post('/unit').success(function(data) {
+            $scope.units = data;
+        });
+        $scope.orderProp = 'age';
+    }]);
+KursachControllers.controller('UnitIdCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var unit_id = $routeParams.unit_id;
+        $http.post('/unit/:unit_id').success(function(data) {
+            $scope.unit = data;
+        });
+        $scope.orderProp = 'age';
+    }]);
+KursachControllers.controller('TaxpayerTaxCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        $http.post('/taxpayer/:id/tax').success(function(data) {
+            $scope.taxes = data;
+        });
+        $scope.orderProp = 'age';
+    }]);
+KursachControllers.controller('TaxpayerTaxIdCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var tax_id = $routeParams.tax_id
+        $http.post('/taxpayer/:id/tax/tax_id').success(function(data) {
+            $scope.tax = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        $http.post('/taxpayer/:id').success(function(data) {
+            $scope.taxpayer = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerObjectsCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        $http.post('/taxpayer/:id/taxitionobject').success(function(data) {
+            $scope.object = data;
+        });
+        $scope.orderProp = 'age';
+    }]);
+KursachControllers.controller('TaxpayerObjectCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var taxition_id = $routeParams.taxition_id;
+        $http.post('/taxpayer/:id/taxitionobject/:taxition_id').success(function(data) {
+            $scope.object = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerPropertiesCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        $http.post('/taxpayer/:id/property').success(function(data) {
+            $scope.properties = data;
+        });
+        $scope.orderProp = 'age';
+    }]);
+KursachControllers.controller('TaxpayerPropertyCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var property_id = $routeParams.property_id;
+        $http.post('/taxpayer/:id/property/:property_id').success(function(data) {
+            $scope.property = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerPaymentsCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var tax_id = $routeParams.tax_id;
+        $http.post('/taxpayer/:id/tax/:tax_id/payment').success(function(data) {
+            $scope.payments = data;
+        });
+        $scope.orderProp = 'age';
+    }]);
+KursachControllers.controller('TaxpayerPaymentCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var tax_id = $routeParams.tax_id;
+        var payment_id = $routeParams.payment_id;
+        $http.post('/taxpayer/:id/tax/:tax_id/payment/:payment_id').success(function(data) {
+            $scope.payment = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerLicencesCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        $http.post('/taxpayer/:id/licence').success(function(data) {
+            $scope.licences = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerLicenceCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var licence_id = $routeParams.licence_id;
+        $http.post('/taxpayer/:id/licence/:licence_id').success(function(data) {
+            $scope.licence = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerCashCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var taxition_id = $routeParams.taxition_id;
+        $http.post('/taxpayer/:id/taxitionobject/:taxition_id/casheregisters').success(function(data) {
+            $scope.cashes = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerChargesCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var tax_id = $routeParams.tax_id;
+        $http.post('/taxpayer/:id/tax/:tax_id/charge').success(function(data) {
+            $scope.charges = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerChargeCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var tax_id = $routeParams.tax_id;
+        var charge_id = $routeParams.charge_id;
+        $http.post('/taxpayer/:id/tax/:tax_id/charge/:charge_id').success(function(data) {
+            $scope.charge = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerAuditsCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        $http.post('/taxpayer/:id/audit').success(function(data) {
+            $scope.audits = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerAuditCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var audit_id = $routeParams.audit_id;
+        $http.post('/taxpayer/:id/audit/:audit_id').success(function(data) {
+            $scope.audit = data;
+        });
+    }]);
+KursachControllers.controller('TaxpayerDecisionCtrl',['$scope','$http',
+    function ($scope, $http) {
+        var id = $routeParams.id;
+        var audit_id = $routeParams.audit_id;
+        $http.post('/taxpayer/:id/audit/:audit_id/decision').success(function(data) {
+            $scope.decision = data;
+        });
+    }]);
 KursachControllers.controller('AuthCtrl',
   function($scope, $auth) {
       $scope.handleRegBtnClick = function() {
